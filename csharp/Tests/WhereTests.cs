1e619c1e754ed716b1c1e21de6234c1f6ce4388a
Neil Sorensen <nsorens@gmail.com>
2020-05-13 18:42:04 -0600

    Set up C#

---
+++ csharp/Tests/WhereTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace LinqReimplemented.Tests
+{
+    public class WhereTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
